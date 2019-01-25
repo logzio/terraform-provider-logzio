db5086d4dfcef6c45ec05267af0789ec47bf5f88
Jon Boydell <jon.boydell@massive.co>
2019-01-25 15:50:16 +0000

    updated terraform examples

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,23 @@ provider "logzio" {
   api_token = "${var.api_token}"
 }
 
+resource "logzio_endpoint" "my_endpoint" {
+  title = "my_endpoint"
+  description = "hello"
+  endpoint_type = "slack"
+  slack {
+    url = "https://this.is.com/some/url"
+  }
+}
+
 resource "logzio_alert" "my_alert" {
   title = "my_other_title"
   query_string = "loglevel:ERROR"
   operation = "GREATER_THAN"
-  notification_emails = ["${var.notification_email}"]
+  notification_emails = []
   search_timeframe_minutes = 5
   value_aggregation_type = "NONE"
-  alert_notification_endpoints = []
+  alert_notification_endpoints = ["${logzio_endpoint.my_endpoint.id}"]
   suppress_notifications_minutes = 5
   severity_threshold_tiers = [
     {
--- terraform/terraform.sh
+++ terraform/terraform.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export TF_LOG=DEBUG
 terraform init
-terraform plan -var-file=./variables/${USER}.tfvars -out terraform.plan
+terraform plan -var-file=./${USER}.tfvars -out terraform.plan
 terraform apply terraform.plan
