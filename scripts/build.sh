1e5675d499b8e4453fdf48b9e64883976db616ab
Jon Boydell <jonboydell@hotmail.com>
2019-07-17 17:23:50 +0100

    small fixes to make the TF example work properly

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash -xe
+#!/usr/bin/env bash
+rm -rf ./build
 go build -o ./build/terraform-provider-logzio
-cp ./build/terraform-provider-logzio ~/.terraform.d/plugins
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,1 @@
 variable "api_token" {}
-variable "notification_email" {}
