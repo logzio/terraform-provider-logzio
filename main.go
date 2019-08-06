cac130fe19790ce136d656fe530dc2cee01a1c73
Jon Boydell <jonboydell@hotmail.com>
2019-08-06 12:02:18 +0100

    package structure conforms to TF provider suggested structure

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/jonboydell/logzio_terraform_provider/logzio"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return logzio.Provider()
 		},
 	})
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash -xe
 go build -o ./build/terraform-provider-logzio
 cp ./build/terraform-provider-logzio ~/.terraform.d/plugins/
+go run utils/template.go
+mkdir -p ~./terraform.d/metadata-repo/terraform/model/providers/
+echo "logzio" >> ~./terraform.d/metadata-repo/terraform/model/providers.list
+cp ./build/logzio.json ~./terraform.d/metadata-repo/terraform/model/providers/
