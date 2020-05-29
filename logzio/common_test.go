71d28776411229b9aecf3f5edee96ffcbaafc5be
Owen Rumney <owen.rumney@form3.tech>
2020-05-29 11:42:23 +0100

    add unit tests for the string whitespace stripper

--- logzio/common_test.go
+++ logzio/common_test.go
@@ -26,7 +26,6 @@ func TestStripWhitespaceReturnsDesiredResultWhenInputStringIsJustSpaces(t *testi
 	assert.EqualValues(t, expected, stripAllWhitespace(input))
 }
 
-
 func TestStripWhitespaceReturnsDesiredResultWhenInputStringIsJustTabs(t *testing.T) {
 	input := "				"
 	expected := ""
@@ -37,6 +36,22 @@ func TestStripWhitespaceReturnsDesiredResultWhenInputStringIsJustTabs(t *testing
 func TestStripWhitespaceReturnsDesiredResultWhenInputStringIsJustCharacters(t *testing.T) {
 	input := "abcdefghijklmnoplqrstuvwxyz"
 	expected := "abcdefghijklmnoplqrstuvwxyz"
+
+	assert.EqualValues(t, expected, stripAllWhitespace(input))
+}
+
+func TestStripWhitespaceReturnsDesiredResultWhenInputStringHasWhiteSpaceAtFrontAndBack(t *testing.T) {
+	input := "     text with whitespace at front and back    	"
+	expected := "textwithwhitespaceatfrontandback"
+
+	assert.EqualValues(t, expected, stripAllWhitespace(input))
+}
+
+func TestStripWhitespaceReturnsDesiredResultWhenInputStringHasCarraigeReturn(t *testing.T) {
+	input := `this string
+	spans multiple
+lines`
+	expected := "thisstringspansmultiplelines"
 
 	assert.EqualValues(t, expected, stripAllWhitespace(input))
 }
