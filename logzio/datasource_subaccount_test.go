63b31d002c66079a76c606a6bb49d6505a452b14
yyyogev <yogev.metzuyanim@logz.io>
2020-07-28 19:22:30 +0300

    acceptance tests with credentials
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- logzio/datasource_subaccount_test.go
+++ logzio/datasource_subaccount_test.go
@@ -1,12 +1,31 @@
 package logzio
 
 import (
+	"fmt"
 	"github.com/hashicorp/terraform/helper/resource"
+	"os"
+	"strconv"
 	"testing"
+)
+
+const (
+	envLogzioEmail = "LOGZIO_EMAIL"
 )
 
 func TestAccDataSourceSubaccount(t *testing.T) {
 	resourceName := "data.logzio_subaccount.subaccount_datasource_by_id"
+	if v := os.Getenv(envLogzioAccountId); v == "" {
+		t.Log(v)
+		t.Fatalf("%s must be set for acceptance tests", envLogzioAccountId)
+	}
+	accountId, _ := strconv.ParseInt(os.Getenv(envLogzioAccountId), BASE_10, BITSIZE_64)
+
+	if v := os.Getenv(envLogzioEmail); v == "" {
+		t.Log(v)
+		t.Fatalf("%s must be set for acceptance tests", envLogzioEmail)
+	}
+	email := os.Getenv(envLogzioEmail)
+	terraformPlan := testAccCheckLogzioSubaccountDatasourceConfig(email, accountId)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
@@ -14,7 +33,7 @@ func TestAccDataSourceSubaccount(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				ExpectNonEmptyPlan: true,
-				Config: ReadFixtureFromFile("create_subaccount_datasource.tf"),
+				Config: terraformPlan,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "account_name", "test"),
 					resource.TestCheckResourceAttr(resourceName, "retention_days", "2"),
@@ -23,4 +42,23 @@ func TestAccDataSourceSubaccount(t *testing.T) {
 			},
 		},
 	})
+}
+
+func testAccCheckLogzioSubaccountDatasourceConfig(email string, accountId int64) string {
+	return fmt.Sprintf(`
+resource "logzio_subaccount" "subaccount_datasource" {
+  email = "%s"
+  account_name = "test"
+  retention_days = 2
+  max_daily_gb = 1
+  sharing_objects_accounts = [
+    %d
+  ]
+}
+
+data "logzio_subaccount" "subaccount_datasource_by_id" {
+  account_id = logzio_subaccount.subaccount_datasource.id
+  depends_on = ["logzio_subaccount.subaccount_datasource"]
+}
+`, email, accountId)
 }
