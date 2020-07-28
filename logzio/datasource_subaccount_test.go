69a75c0ef31bc9649096103f8f856a5915adb518
yyyogev <yogev.metzuyanim@logz.io>
2020-07-28 18:00:32 +0300

    add subaccount datasource test
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- logzio/datasource_subaccount_test.go
+++ logzio/datasource_subaccount_test.go
@@ -1,1 +1,26 @@
 package logzio
+
+import (
+	"github.com/hashicorp/terraform/helper/resource"
+	"testing"
+)
+
+func TestAccDataSourceSubaccount(t *testing.T) {
+	resourceName := "data.logzio_subaccount.subaccount_datasource_by_id"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				ExpectNonEmptyPlan: true,
+				Config: ReadFixtureFromFile("create_subaccount_datasource.tf"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_name", "test"),
+					resource.TestCheckResourceAttr(resourceName, "retention_days", "2"),
+					resource.TestCheckResourceAttr(resourceName, "max_daily_gb", "1"),
+				),
+			},
+		},
+	})
+}
--- logzio/testdata/fixtures/create_subaccount_datasource.tf
+++ logzio/testdata/fixtures/create_subaccount_datasource.tf
@@ -1,17 +1,14 @@
-variable "api_token" {
-  type = "string"
-  description = "your logzio API token"
-}
-
-provider "logzio" {
-  api_token = "${var.api_token}"
-}
-
-resource "logzio_subaccount" "my_subaccount" {
-  email = "user@logz.io"
+resource "logzio_subaccount" "subaccount_datasource" {
+  email = "your@email.com"
   account_name = "test"
   retention_days = 2
+  max_daily_gb = 1
   sharing_objects_accounts = [
     12345
   ]
+}
+
+data "logzio_subaccount" "subaccount_datasource_by_id" {
+  account_id = logzio_subaccount.subaccount_datasource.id
+  depends_on = ["logzio_subaccount.subaccount_datasource"]
 }
