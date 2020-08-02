9b2e8d27da87770b2370e9c792b8ec2a8f2f717f
yyyogev <yogev.metzuyanim@logz.io>
2020-08-02 12:32:06 +0300

    scripts
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash -xe
 TERRAFORM_HOME=~/.terraform.d
 
-GO111MODULE=on go get -v -t ./...
+#GO111MODULE=on go get -v -t ./...
 GO111MODULE=on go build -o ./build/terraform-provider-logzio
 cp ./build/terraform-provider-logzio ${TERRAFORM_HOME}/plugins/
 
--- scripts/build_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t logzio_terraform_provider:build .
-docker run -v ~/go/src/github.com/jonboydell/logzio_terraform_provider:/go/src/github.com/jonboydell/logzio_terraform_provider logzio_terraform_provider:build go get -v ./...
