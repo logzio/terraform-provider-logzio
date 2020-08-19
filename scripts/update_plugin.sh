1096e9e84f4663195059101a4623b009ed68f637
Richard Pablo <richardpablop@gmail.com>
2020-08-19 18:14:58 +0100

    Removing Release_ since the URL is back to normal

--- scripts/update_plugin.sh
+++ scripts/update_plugin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PROVIDER_VERSION=v1.1.4
-DOWNLOAD_URL=https://github.com/logzio/logzio_terraform_provider/releases/download/Realse_${PROVIDER_VERSION}
+DOWNLOAD_URL=https://github.com/logzio/logzio_terraform_provider/releases/download/${PROVIDER_VERSION}
 
 rm -f ~/.terraform.d/plugins/terraform-provider-logzio
 mkdir -p  ~/.terraform.d/plugins/
