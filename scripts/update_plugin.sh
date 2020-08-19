4717ed002783307bb4a637482e75759e9886bedd
Richard Pablo <richardpablop@gmail.com>
2020-08-19 14:13:03 +0100

    Adding commands for new users
    Adding "Realse_" to the download URL
    Adding `-f ` on the `rm` command to ignore if the file doesn't exists.
    Adding `mkdir -p  $HOME/.terraform.d/plugins/` in case the user has never installed a plugin
    Changing `$HOME` by `~` to be consistent

--- scripts/update_plugin.sh
+++ scripts/update_plugin.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 PROVIDER_VERSION=v1.1.4
-DOWNLOAD_URL=https://github.com/logzio/logzio_terraform_provider/releases/download/${PROVIDER_VERSION}
+DOWNLOAD_URL=https://github.com/logzio/logzio_terraform_provider/releases/download/Realse_${PROVIDER_VERSION}
 
-rm ~/.terraform.d/plugins/terraform-provider-logzio
-wget ${DOWNLOAD_URL}/terraform-provider-logzio_${PROVIDER_VERSION}_darwin_amd64 -O $HOME/.terraform.d/plugins/terraform-provider-logzio
+rm -f ~/.terraform.d/plugins/terraform-provider-logzio
+mkdir -p  ~/.terraform.d/plugins/
+wget ${DOWNLOAD_URL}/terraform-provider-logzio_${PROVIDER_VERSION}_darwin_amd64 -O ~/.terraform.d/plugins/terraform-provider-logzio
 chmod +x ~/.terraform.d/plugins/terraform-provider-logzio
