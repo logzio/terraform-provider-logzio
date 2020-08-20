b74cbded8de03602d64ec189374941b2a6e32c3e
Richard Pablo <richardpablop@gmail.com>
2020-08-20 10:51:11 +0100

    Adding commands for new users (#44)
    * Adding commands for new users
    Adding `-f ` on the `rm` command to ignore if the file doesn't exists.
    Adding `mkdir -p  $HOME/.terraform.d/plugins/` in case the user has never installed a plugin
    Changing `$HOME` by `~` to be consistent

--- scripts/update_plugin.sh
+++ scripts/update_plugin.sh
@@ -2,6 +2,7 @@
 PROVIDER_VERSION=v1.1.4
 DOWNLOAD_URL=https://github.com/logzio/logzio_terraform_provider/releases/download/${PROVIDER_VERSION}
 
-rm ~/.terraform.d/plugins/terraform-provider-logzio
-wget ${DOWNLOAD_URL}/terraform-provider-logzio_${PROVIDER_VERSION}_darwin_amd64 -O $HOME/.terraform.d/plugins/terraform-provider-logzio
+rm -f ~/.terraform.d/plugins/terraform-provider-logzio
+mkdir -p  ~/.terraform.d/plugins/
+wget ${DOWNLOAD_URL}/terraform-provider-logzio_${PROVIDER_VERSION}_darwin_amd64 -O ~/.terraform.d/plugins/terraform-provider-logzio
 chmod +x ~/.terraform.d/plugins/terraform-provider-logzio
