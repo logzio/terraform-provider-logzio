8f1fb900a2e3728af481e959222d7702f8e1c042
Yogev Mets <yogev.metzuyanim@logz.io>
2020-11-11 13:18:06 +0200

    Docs (#53)
    * simplify get, build and run
    
    * bump version 1.14
    
    * api host
    
    * changelog
    
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,3 @@ TERRAFORM_HOME=~/.terraform.d
 #GO111MODULE=on go get -v -t ./...
 GO111MODULE=on go build -o ./build/terraform-provider-logzio
 cp ./build/terraform-provider-logzio ${TERRAFORM_HOME}/plugins/
-
-go run utils/template.go
-mkdir -p ${TERRAFORM_HOME}/metadata-repo/terraform/model/providers/
-echo "logzio" >> ${TERRAFORM_HOME}/metadata-repo/terraform/model/providers.list
-cp ./build/logzio.json ${TERRAFORM_HOME}/metadata-repo/terraform/model/providers/
