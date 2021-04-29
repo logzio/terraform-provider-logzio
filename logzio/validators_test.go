3e30bb0cc6dbee1ecc92a7d249a7b86287ba561a
mirii1994 <miri.ignatiev@logz.io>
2021-04-29 18:32:47 +0300

    add validator tests for output type & sort

--- logzio/validators_test.go
+++ logzio/validators_test.go
@@ -2,6 +2,7 @@ package logzio
 
 import (
 	"github.com/logzio/logzio_terraform_client/alerts"
+	"github.com/logzio/logzio_terraform_client/alerts_v2"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -40,4 +41,48 @@ func TestValidUrl(t *testing.T) {
 	str := "https://some.url"
 	_, errors := validateUrl(str, "url")
 	assert.Len(t, errors, 0)
+}
+
+func TestValidateOutputTypes(t *testing.T) {
+	validOptions := []string {
+		alerts_v2.OutputTypeJson,
+		alerts_v2.OutputTypeTable,
+	}
+
+	for _, s := range validOptions {
+		_, errors := validateOutputType(s, "output_type")
+		assert.Empty(t, errors)
+	}
+
+	invalidNames := []string{
+		"",
+		"this is not a valid type",
+	}
+
+	for _, s := range invalidNames {
+		_, errors := validateOutputType(s, "output_type")
+		assert.NotEmpty(t, errors)
+	}
+}
+
+func TestValidateSortTypes(t *testing.T) {
+	validTypes := []string{
+		alerts_v2.SortAsc,
+		alerts_v2.SortDesc,
+	}
+
+	for _, s := range validTypes {
+		_, errors := validateSortTypes(s, "sort")
+		assert.Empty(t, errors)
+	}
+
+	invalidNames := []string{
+		"",
+		"this is not a valid type",
+	}
+
+	for _, s := range invalidNames {
+		_, errors := validateSortTypes(s, "sort")
+		assert.NotEmpty(t, errors)
+	}
 }
