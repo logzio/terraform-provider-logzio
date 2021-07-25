c3b958a505fb58e3799735ce8b7f7382333ff76d
mirii1994 <miri.ignatiev@logz.io>
2021-07-25 11:01:40 +0300

    add data source negative test

--- logzio/datasource_drop_filter_test.go
+++ logzio/datasource_drop_filter_test.go
@@ -3,6 +3,7 @@ package logzio
 import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"regexp"
 	"testing"
 )
 
@@ -64,6 +65,28 @@ func TestAccDataSourceDropFilterByAttributes(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceDropFilterNotExist(t *testing.T) {
+	filterNameResource := "test_create_drop_filter"
+	resourceName := "logzio_drop_filter." + filterNameResource
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheckApiToken(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: resourceTestDropFilter(filterNameResource, dropFilterResourceCreateDropFilter),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, dropFilterLogType, "some_type"),
+					resource.TestCheckResourceAttr(resourceName, dropFilterFieldConditions+".#", "2"),
+				),
+			},
+			{
+				Config: testAccKubernetesDataSourceDropFilterByIdNotExists(),
+				ExpectError: regexp.MustCompile("couldn't find drop filter with specified attributes"),
+			},
+		},
+	})
+}
+
 func testAccKubernetesDataSourceDropFilterById() string {
 	return fmt.Sprintf(`data "logzio_drop_filter" "my_drop_filter_datasource" {
 drop_filter_id = "${logzio_drop_filter.test_create_drop_filter.id}"
@@ -83,6 +106,13 @@ func testAccKubernetesDataSourceDropFilterByAttributes() string {
     field_name = "another_field"
     value = 200
   }
+}
+`)
+}
+
+func testAccKubernetesDataSourceDropFilterByIdNotExists() string {
+	return fmt.Sprintf(`data "logzio_drop_filter" "my_drop_filter_datasource" {
+drop_filter_id = "1234"
 }
 `)
 }
