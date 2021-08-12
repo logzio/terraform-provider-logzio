ea5969a8b2575c07209e81163cae473e660a8282
mirii1994 <miri.ignatiev@logz.io>
2021-08-12 15:44:25 +0300

    github actions only on pr

--- logzio/testdata/fixtures/create_slack_endpoint.tf
+++
@@ -1,8 +0,0 @@
-resource "logzio_endpoint" "%s" {
-  endpoint_type = "Slack"
-  title = "slack_endpoint"
-  description = "some valid description"
-  slack {
-    url = "https://www.test.com"
-  }
-}
