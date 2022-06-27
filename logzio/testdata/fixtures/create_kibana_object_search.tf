5c9378ff9a5bc2cd6d45297f19202184c165d9df
mirii1994 <miri.levi94@gmail.com>
2022-06-27 17:21:32 +0300

    fix path for create search

--- logzio/testdata/fixtures/create_kibana_object_search.tf
+++ logzio/testdata/fixtures/create_kibana_object_search.tf
@@ -1,4 +1,4 @@
 resource "logzio_kibana_object" "test_kb_obj_search" {
   kibana_version = "7.2.1"
-  data = file("./create_search.json")
+  data = file("./testdata/fixtures/kibana_objects/create_search.json")
 }
