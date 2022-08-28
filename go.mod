83ee70a73bb472d6f23596431b99f971f87f79de
Miri Bar <37478344+mirii1994@users.noreply.github.com>
2022-08-28 16:57:57 +0300

    update provider sdk in go.sum, update go version, update logzio client version (#120)

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
 module github.com/logzio/logzio_terraform_provider
 
-go 1.16
+go 1.18
 
 require (
 	github.com/avast/retry-go v3.0.0+incompatible
-	github.com/hashicorp/terraform-plugin-sdk v1.7.0
-	github.com/logzio/logzio_terraform_client v1.11.0
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.21.0
+	github.com/logzio/logzio_terraform_client v1.12.0
 	github.com/stretchr/testify v1.7.0
 )
--- logzio/provider_test.go
+++ logzio/provider_test.go
@@ -4,32 +4,37 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 var (
 	testAccExpectedAlertChannelName string
 	testAccExpectedApplicationName  string
-	testAccProviders                map[string]terraform.ResourceProvider
-	testAccProvider                 *schema.Provider
+	testAccProviderFactories        = map[string]func() (*schema.Provider, error){
+		"kubernetes": func() (*schema.Provider, error) {
+			return Provider(), nil
+		},
+	}
+	testAccProvider *schema.Provider
 )
 
 func init() {
-	testAccProvider = Provider().(*schema.Provider)
-	testAccProviders = map[string]terraform.ResourceProvider{
-		"logzio": testAccProvider,
+	testAccProvider = Provider()
+	testAccProviderFactories = map[string]func() (*schema.Provider, error){
+		"logzio": func() (*schema.Provider, error) {
+			return Provider(), nil
+		},
 	}
 }
 
 func TestProvider(t *testing.T) {
-	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+	if err := Provider().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 }
 
 func TestProviderImpl(t *testing.T) {
-	var _ terraform.ResourceProvider = Provider()
+	var _ *schema.Provider = Provider()
 }
 
 func testAccPreCheckEnv(t *testing.T, env string) {
