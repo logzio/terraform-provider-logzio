44db2fe8f0d1bc374fbfc80e00a188b9c98aa7ca
mirii1994 <miri.ignatiev@logz.io>
2022-09-19 13:21:24 +0300

    update ds alerts v2 + tests

--- logzio/datasource_alert_v2_test.go
+++ logzio/datasource_alert_v2_test.go
@@ -1,7 +1,7 @@
 package logzio
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/logzio/logzio_terraform_provider/logzio/utils"
 	"testing"
 )
@@ -11,12 +11,15 @@ func TestAccDataSourceLogzIoAlertV2(t *testing.T) {
 	defer utils.SleepAfterTest()
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheckApiToken(t) },
-		Providers: testAccProviders,
+		PreCheck:          func() { testAccPreCheckApiToken(t) },
+		ProviderFactories: testAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				ExpectNonEmptyPlan: true,
-				Config:             utils.ReadFixtureFromFile("create_alert_v2_datasource.tf"),
+				Config:  getConfigResourceAlertV2(),
+				Destroy: false,
+			},
+			{
+				Config: getConfigResourceAlertV2() + getConfigDatasourceAlertV2(),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "title", "hello"),
 					resource.TestCheckResourceAttr(resourceName, "sub_components.#", "1"),
@@ -29,4 +32,55 @@ func TestAccDataSourceLogzIoAlertV2(t *testing.T) {
 			},
 		},
 	})
+}
+
+func getConfigResourceAlertV2() string {
+	return `resource "logzio_alert_v2" "alert_v2_datasource" {
+  title = "hello"
+  description = "this is a description"
+  tags = [
+    "some",
+    "test"]
+  search_timeframe_minutes = 5
+  is_enabled = false
+  notification_emails = [
+    "testx@test.com"]
+  suppress_notifications_minutes = 5
+  output_type = "JSON"
+  sub_components {
+    query_string = "loglevel:ERROR"
+    should_query_on_all_accounts = true
+    operation = "EQUALS"
+    value_aggregation_type = "COUNT"
+    severity_threshold_tiers {
+      severity = "HIGH"
+      threshold = 10
+    }
+    filter_must = jsonencode([
+      {
+        match_phrase: {
+          "some_field": {
+            "query": "some_query"
+          }
+        }
+      },
+      {
+        another_match: {
+          "some_field2": {
+            "query": "hello world"
+          }
+        }
+      }
+    ])
+  }
+}
+`
+}
+
+func getConfigDatasourceAlertV2() string {
+	return `data "logzio_alert_v2" "alert_v2_datasource_by_id" {
+  id = "${logzio_alert_v2.alert_v2_datasource.id}"
+  depends_on = ["logzio_alert_v2.alert_v2_datasource"]
+}
+`
 }
