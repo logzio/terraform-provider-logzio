f55d419e70724b0833b1f4e091f1295012d0a129
Miri Bar <37478344+mirii1994@users.noreply.github.com>
2022-09-19 16:48:10 +0300

    refactor ds auth groups (#138)

--- logzio/datasource_authentication_groups.go
+++ logzio/datasource_authentication_groups.go
@@ -1,7 +1,9 @@
 package logzio
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"context"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/logzio/logzio_terraform_client/authentication_groups"
 	"math/rand"
 	"strconv"
@@ -14,7 +16,7 @@ const (
 
 func dataSourceAuthenticationGroups() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceAuthenticationGroupsRead,
+		ReadContext: dataSourceAuthenticationGroupsRead,
 		Schema: map[string]*schema.Schema{
 			authGroupsAuthGroup: {
 				Type:     schema.TypeList,
@@ -36,10 +38,10 @@ func dataSourceAuthenticationGroups() *schema.Resource {
 	}
 }
 
-func dataSourceAuthenticationGroupsRead(d *schema.ResourceData, m interface{}) error {
+func dataSourceAuthenticationGroupsRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	groups, err := getAuthGroups(authGroupsDatasourceRetries, m)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	if len(groups) > 0 {
@@ -47,7 +49,7 @@ func dataSourceAuthenticationGroupsRead(d *schema.ResourceData, m interface{}) e
 		rand.Seed(time.Now().UnixNano())
 		id := rand.Int()
 		d.SetId(strconv.FormatInt(int64(id), 10))
-		setAuthenticationGroupsDatasource(id, groups, d)
+		setAuthenticationGroupsDatasource(groups, d)
 	}
 
 	return nil
@@ -63,12 +65,8 @@ func getAuthGroups(retries int, m interface{}) ([]authentication_groups.Authenti
 	return groups, err
 }
 
-func setAuthenticationGroupsDatasource(id int, groups []authentication_groups.AuthenticationGroup, d *schema.ResourceData) {
+func setAuthenticationGroupsDatasource(groups []authentication_groups.AuthenticationGroup, d *schema.ResourceData) {
 	var groupsToSchema []map[string]interface{}
-
-	if id != 0 {
-		d.Set(authGroupsId, id)
-	}
 
 	for _, group := range groups {
 		groupMap := map[string]interface{}{
--- logzio/datasource_authentication_groups_test.go
+++ logzio/datasource_authentication_groups_test.go
@@ -2,7 +2,7 @@ package logzio
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/logzio/logzio_terraform_client/authentication_groups"
 	"github.com/logzio/logzio_terraform_provider/logzio/utils"
 	"testing"
@@ -21,8 +21,8 @@ func TestAccDataSourceAuthenticationGroups(t *testing.T) {
 	defer utils.SleepAfterTest()
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheckApiToken(t) },
-		Providers: testAccProviders,
+		PreCheck:          func() { testAccPreCheckApiToken(t) },
+		ProviderFactories: testAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: getAuthGroupsConfig(resourceName, userRolesInConfigCreate),
