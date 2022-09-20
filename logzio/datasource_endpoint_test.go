fbfd13d4bb5f073599f84343a57f7826741a6324
Miri Bar <37478344+mirii1994@users.noreply.github.com>
2022-09-20 10:07:25 +0300

    refactor ds endpoint + tests (#140)

--- logzio/datasource_endpoint_test.go
+++ logzio/datasource_endpoint_test.go
@@ -1,7 +1,7 @@
 package logzio
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/logzio/logzio_terraform_provider/logzio/utils"
 	"regexp"
 	"testing"
@@ -11,11 +11,10 @@ func TestAccDataSourceEndpoint(t *testing.T) {
 	defer utils.SleepAfterTest()
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheckApiToken(t) },
-		Providers: testAccProviders,
+		PreCheck:          func() { testAccPreCheckApiToken(t) },
+		ProviderFactories: testAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				ExpectNonEmptyPlan:        true,
 				Config:                    utils.ReadFixtureFromFile("valid_slack_endpoint_datasource.tf"),
 				PreventPostDestroyRefresh: true,
 				Check: resource.ComposeAggregateTestCheckFunc(
