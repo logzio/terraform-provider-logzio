caaf704fd94b18d8d72a7527e3354c3970d06462
mirii1994 <miri.ignatiev@logz.io>
2022-09-20 10:14:40 +0300

    refactor ds kibana objects

--- logzio/datasource_kibana_object.go
+++ logzio/datasource_kibana_object.go
@@ -1,9 +1,11 @@
 package logzio
 
 import (
+	"context"
 	"fmt"
 	"github.com/avast/retry-go"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/logzio/logzio_terraform_client/kibana_objects"
 	"strings"
 	"time"
@@ -16,7 +18,7 @@ const (
 
 func dataSourceKibanaObject() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceKibanaObjectRead,
+		ReadContext: dataSourceKibanaObjectRead,
 		Schema: map[string]*schema.Schema{
 			kibanaObjectDatasourceIdField: {
 				Type:     schema.TypeString,
@@ -44,12 +46,12 @@ func dataSourceKibanaObject() *schema.Resource {
 	}
 }
 
-func dataSourceKibanaObjectRead(d *schema.ResourceData, m interface{}) error {
+func dataSourceKibanaObjectRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	client, _ := kibana_objects.New(m.(Config).apiToken, m.(Config).baseUrl)
 	kbObjId := d.Get(kibanaObjectDatasourceIdField).(string)
 	kbObjType, err := getKibanaObjectType(d)
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
 
 	err = retry.Do(
@@ -83,7 +85,11 @@ func dataSourceKibanaObjectRead(d *schema.ResourceData, m interface{}) error {
 			}),
 	)
 
-	return err
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
 }
 
 func getKibanaObjectType(d *schema.ResourceData) (*kibana_objects.ExportType, error) {
