1d775f76f7b46eb395fe153c198ed963ceb471de
mirii1994 <miri.ignatiev@logz.io>
2022-09-28 11:13:53 +0300

    fix test ds kibana object

--- logzio/testdata/fixtures/kibana_object_datasource.tf
+++ logzio/testdata/fixtures/kibana_object_datasource.tf
@@ -1,8 +1,3 @@
-resource "logzio_kibana_object" "test_kb_for_datasource" {
-  kibana_version = "7.2.1"
-  data = file("./testdata/fixtures/kibana_objects/create_search.json")
-}
-
 data "logzio_kibana_object" "ds_kb" {
   object_id = "search:tf-provider-test-search"
   object_type = "search"
