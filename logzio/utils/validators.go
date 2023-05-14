50d42741be3ae2b795e88b9f2269287762d1dc3a
mirii1994 <miri.ignatiev@logz.io>
2023-05-14 11:36:18 +0300

    upgrade logzio client in go mod, remove alert v1 utils functions and tests

--- logzio/utils/validators.go
+++ logzio/utils/validators.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/logzio/logzio_terraform_client/alerts"
 	"github.com/logzio/logzio_terraform_client/alerts_v2"
 	"github.com/logzio/logzio_terraform_client/archive_logs"
 	"github.com/logzio/logzio_terraform_client/authentication_groups"
@@ -22,36 +21,6 @@ func contains(slice []string, s string) bool {
 		}
 	}
 	return false
-}
-
-func ValidateOperation(v interface{}, k string) (ws []string, errors []error) {
-
-	value := v.(string)
-
-	validOperations := []string{
-		alerts.OperatorGreaterThanOrEquals,
-		alerts.OperatorGreaterThan,
-		alerts.OperatorEquals,
-		alerts.OperatorLessThan,
-		alerts.OperatorLessThanOrEquals,
-		alerts.OperatorNotEquals,
-	}
-
-	if len(value) == 0 {
-		errors = append(errors, fmt.Errorf("operation %q must not be blank and be one of %s", k, validOperations))
-	}
-
-	valid := false
-	for _, op := range validOperations {
-		if op == value {
-			valid = true
-		}
-	}
-
-	if !valid {
-		errors = append(errors, fmt.Errorf("operation %q must be one of %s", k, validOperations))
-	}
-	return
 }
 
 func ValidateOperationV2(v interface{}, k string) (ws []string, errors []error) {
@@ -80,42 +49,6 @@ func ValidateOperationV2(v interface{}, k string) (ws []string, errors []error) 
 
 	if !valid {
 		errors = append(errors, fmt.Errorf("operation %q must be one of %s", k, validOperations))
-	}
-	return
-}
-
-func validAggregationTypes(v interface{}, k string) (ws []string, errors []error) {
-
-	value := v.(string)
-
-	validAggregationTypes := []string{
-		alerts.AggregationTypeUniqueCount,
-		alerts.AggregationTypeAvg,
-		alerts.AggregationTypeMax,
-		alerts.AggregationTypeNone,
-		alerts.AggregationTypeSum,
-		alerts.AggregationTypeCount,
-		alerts.AggregationTypeMin,
-	}
-
-	if !contains(validAggregationTypes, value) {
-		errors = append(errors, fmt.Errorf("valueAggregationType %q must be one of %s", k, validAggregationTypes))
-	}
-	return
-}
-
-func validateSeverityTypes(v interface{}, k string) (ws []string, errors []error) {
-
-	value := v.(string)
-
-	validSeverityTypes := []string{
-		alerts.SeverityHigh,
-		alerts.SeverityMedium,
-		alerts.SeverityHigh,
-	}
-
-	if !contains(validSeverityTypes, value) {
-		errors = append(errors, fmt.Errorf("validSeverityType %q must be one of %s", k, validSeverityTypes))
 	}
 	return
 }
