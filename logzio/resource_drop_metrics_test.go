87861ccc3be9b9f800e7608f15abd99790863118
Yotam loewenbach <yotam.loewenbach@logz.io>
2025-08-26 13:29:11 +0300

    `dropMetricsName` tests

--- logzio/resource_drop_metrics_test.go
+++ logzio/resource_drop_metrics_test.go
@@ -2,23 +2,27 @@ package logzio
 
 import (
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/logzio/logzio_terraform_provider/logzio/utils"
 	"log"
 	"os"
 	"regexp"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/logzio/logzio_terraform_provider/logzio/utils"
 )
 
 const (
 	dropMetricResourceCreateSimple           = "create_drop_metrics_simple"
 	dropMetricResourceCreateComplex          = "create_drop_metrics_complex"
+	dropMetricResourceCreateWithName         = "create_drop_metrics_with_name"
+	dropMetricResourceCreateNameTooLong      = "create_drop_metrics_name_too_long"
 	dropMetricResourceCreateNoLabelName      = "create_drop_metrics_missing_label_name"
 	dropMetricResourceCreateNoValue          = "create_drop_metrics_missing_value"
 	dropMetricResourceCreateEmptyCondition   = "create_drop_metrics_empty_condition"
 	dropMetricResourceCreateInvalidCondition = "create_drop_metrics_invalid_condition"
 	dropMetricResourceCreateInvalidOperator  = "create_drop_metrics_invalid_operator"
 	dropMetricResourceUpdate                 = "update_drop_metrics"
+	dropMetricResourceUpdateWithName         = "update_drop_metrics_with_name"
 )
 
 func TestAccLogzioDropMetric_CreateDropMetricSimple(t *testing.T) {
@@ -39,6 +43,7 @@ func TestAccLogzioDropMetric_CreateDropMetricSimple(t *testing.T) {
 				Config: resourceTestDropMetrics(filterName, dropMetricResourceCreateSimple, accountId),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, dropMetricsAccountId, accountId),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsName, ""),
 					resource.TestCheckResourceAttr(resourceName, dropMetricsFilters+".#", "1"),
 					resource.TestCheckResourceAttr(resourceName, dropMetricsFilterOperator, "AND"),
 					resource.TestCheckResourceAttr(resourceName, dropMetricsActive, "true"),
@@ -288,6 +293,98 @@ func TestAccLogzioDropMetric_UpdateDropMetricsDisable(t *testing.T) {
 				Config: resourceTestDropMetrics(filterName, dropMetricResourceUpdate, accountId),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, dropMetricsAccountId, accountId),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsFilters+".#", "2"),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsFilterOperator, "AND"),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsActive, "false"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccLogzioDropMetric_CreateDropMetricWithName(t *testing.T) {
+	filterName := "test_create_drop_metrics_with_name"
+	resourceName := "logzio_drop_metrics." + filterName
+	accountId := os.Getenv(envLogzioMetricsAccountId)
+
+	defer utils.SleepAfterTest()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckApiToken(t)
+			testAccPreCheckMetricsAccountId(t)
+		},
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: resourceTestDropMetrics(filterName, dropMetricResourceCreateWithName, accountId),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, dropMetricsAccountId, accountId),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsName, "test-drop-metrics-filter"),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsFilters+".#", "1"),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsFilterOperator, "AND"),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsActive, "true"),
+				),
+			},
+			{
+				Config:            resourceTestDropMetrics(filterName, dropMetricResourceCreateWithName, accountId),
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccLogzioDropMetric_CreateDropMetricNameTooLong(t *testing.T) {
+	filterName := "test_create_drop_metrics_name_too_long"
+	accountId := os.Getenv(envLogzioMetricsAccountId)
+
+	defer utils.SleepAfterTest()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckApiToken(t)
+			testAccPreCheckMetricsAccountId(t)
+		},
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      resourceTestDropMetrics(filterName, dropMetricResourceCreateNameTooLong, accountId),
+				ExpectError: regexp.MustCompile("expected length of name to be in the range \\(0 - 256\\)"),
+			},
+		},
+	})
+}
+
+func TestAccLogzioDropMetric_UpdateDropMetricsWithName(t *testing.T) {
+	filterName := "test_update_drop_metrics_with_name"
+	resourceName := "logzio_drop_metrics." + filterName
+	accountId := os.Getenv(envLogzioMetricsAccountId)
+	defer utils.SleepAfterTest()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckApiToken(t)
+			testAccPreCheckMetricsAccountId(t)
+		},
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: resourceTestDropMetrics(filterName, dropMetricResourceCreateSimple, accountId),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, dropMetricsAccountId, accountId),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsName, ""),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsFilters+".#", "1"),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsFilterOperator, "AND"),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsActive, "true"),
+				),
+			},
+			{
+				Config: resourceTestDropMetrics(filterName, dropMetricResourceUpdateWithName, accountId),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, dropMetricsAccountId, accountId),
+					resource.TestCheckResourceAttr(resourceName, dropMetricsName, "updated-drop-metrics-filter"),
 					resource.TestCheckResourceAttr(resourceName, dropMetricsFilters+".#", "2"),
 					resource.TestCheckResourceAttr(resourceName, dropMetricsFilterOperator, "AND"),
 					resource.TestCheckResourceAttr(resourceName, dropMetricsActive, "false"),
