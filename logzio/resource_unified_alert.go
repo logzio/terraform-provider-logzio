7ca223295e450e3049cd2f2b5d6d6b49b42a0722
Yotam loewenbach <yotam.loewenbach@logz.io>
2026-02-19 16:41:20 +0700

    queryDefinitionAggregation: Computed: true,

--- logzio/resource_unified_alert.go
+++ logzio/resource_unified_alert.go
@@ -380,6 +380,7 @@ func resourceSubComponent() *schema.Resource {
 						queryDefinitionAggregation: {
 							Type:     schema.TypeList,
 							Optional: true,
+							Computed: true,
 							MaxItems: 1,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
